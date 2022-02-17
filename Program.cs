cb9b5d61c7c5a0f15814715b28ac474040863d89
Bhavyashree07 <97940064+Bhavyashree07@users.noreply.github.com>
2022-02-17 12:24:58 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloConsoleApp1
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
